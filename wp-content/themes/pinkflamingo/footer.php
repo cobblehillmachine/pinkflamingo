6286c924afe60a3f623db18edd6634114b81a41b
samanthavandapuye <samantha.vandapuye@gmail.com>
2013-08-12 17:23:07 -0400

    urls fix

--- wp-content/themes/pinkflamingo/footer.php
+++ wp-content/themes/pinkflamingo/footer.php
@@ -14,7 +14,7 @@
 		</div>
 		<div id="logo-cont">
 			<div class="mid-cont">
-				<a href="http://varnadoreelectric.local" class="bottom-logos">
+				<a href="<?php echo get_site_url(2); ?>" class="bottom-logos">
 					<div class="box-hover">
 						<div class="box-title">Sexy, Intimate Event Lighting</div>
 						<div class="box-content">Large format and custom lighting to create the moment, a night sky of small wattage bulbs to set the mood and a splash of color to define the theme for your event or wedding.</div>
@@ -22,7 +22,7 @@
 					</div>
 					<img src="<?php echo get_template_directory_uri(); ?>/images/varnadoreelectric-logo.jpg" />
 				</a>
-				<a href="http://oneifbyland.local" class="bottom-logos">
+				<a href="<?php echo get_site_url(3); ?>" class="bottom-logos">
 					<div class="box-hover">
 						<div class="box-title">LANTERNS, CANDLES, AND CANDELABRA</div>
 						<div class="box-content">Lanterns and Candelabra in a dozen sizes and a variety of textures and shapes – reflective chrome, painted metal and aged wood. Available with a traditional flame candle or a flameless candle.</div>
@@ -38,7 +38,7 @@
 					</div>
 					<img src="<?php echo get_template_directory_uri(); ?>/images/stuartlaurence-logo.jpg" />
 				</a>
-				<a href="http://linensunfurled.local" class="bottom-logos">
+				<a href="<?php echo get_site_url(4); ?>" class="bottom-logos">
 					<div class="box-hover">
 						<div class="box-title">luxury linens that define <br/>your wedding</div>
 						<div class="box-content">Break away from the expected solid colors and shimmers to choose a rich and personalized linen design for your next event or upcoming wedding. A prism of linen colors and patterns ensures that any table designed by Linens Unfurled will be easy to coordinate</div>
@@ -51,7 +51,7 @@
 		</div>
 		<div id="footer">
 			<div class="mid-cont">
-				<a id="copyright" href="http://pinkflamingo.local">&copy; 2013  PINK FLAMINGO GROUP</a>            
+				<a id="copyright" href="<?php echo get_site_url(1); ?>">&copy; 2013  PINK FLAMINGO GROUP</a>            
 				<a id="footer-email" href="mailto:gopink@pinkflamingogroup.com">GOPINK@PINKFLAMINGOGROUP.COM</a>
 				<div id="social-cont">
 					<a class="social-link" href="http://pinterest.com/pinkflamingogrp" target="_blank">pinterest</a>
